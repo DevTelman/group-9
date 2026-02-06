f1fd184cad0b7c5e641dd16640e9831fdfda74f1
anakhasyanelen-afk <anakhasyanelen@gmail.com>
2026-02-06 04:03:01 +0400

    maluna

---
+++ Assets/Audio/SCRIPT/Malunawalking.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Malunawalking : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+       spriteRenderer = GetComponent<SpriteRenderer>();
+       anim = GetComponent<Animator>();
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(moveInput*moveSpeed*Time.deltaTime,0,0);
+        anim.SetBool("isWalking",moveInput !=0);
+    
+         rb.MovePosition(transform.position + movement);
+        if(moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveInput > 0)
+        {
+            spriteRenderer.flipX =false;
+        }
+    }
+}
